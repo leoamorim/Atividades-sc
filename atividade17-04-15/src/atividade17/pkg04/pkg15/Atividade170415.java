0f05ee832cf371359ac0b4f95fe85034809c76f0
leonardo.amorim <leonardo.amorim@WKS07SAVE123.SAVE.NET>
2015-04-27 16:48:30 -0300

    Submetendo arquivo no repositorio local

---
+++ atividade17-04-15/src/atividade17/pkg04/pkg15/Atividade170415.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package atividade17.pkg04.pkg15;
+
+/**
+ *
+ * @author leonardo.amorim
+ */
+public class Atividade170415 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // teste para controle de versão
+    }
+    
+}
